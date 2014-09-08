c734626732134946602f39634675885c6cf334d6
Pierre Ferré <pierre@pierreferre.com>
2014-09-08 23:48:18 +0100

    not empty

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -5,7 +5,7 @@ use Domains\Mailer\Email;
 $domains = new Domains\Check;
 $forRenewals = $domains->getRenewalDates();
 
-if (empty($forRenewals)) {
+if (!empty($forRenewals)) {
 	$email = new Email;
 	$email->sendMessage($forRenewals);
 }
