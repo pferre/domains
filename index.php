fc589545aa5f096902d98ed81e815ddbdff0b68c
Pierre Ferré <pierre@pierreferre.com>
2015-01-24 13:07:14 +0000

    bootstrap

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
-require_once 'vendor/autoload.php';
-require_once 'config/database.php';
 require_once 'bootstrap/start.php';
+
+use Domains\Mailer\Email;
+use Domains\DomainsChecker;
+
+$domains = new DomainsChecker();
+$domainsForRenewals = $domains->getRenewalDates();
+
+if ( !empty($domainsForRenewals ) ) {
+
+    $email = new Email();
+    $email->send($domainsForRenewals);
+    exit;
+
+}
